bf8c59cf0b3db1149f163dbc0ba0245ca08d0148
Stephenlaye2 <32752271+Stephenlaye2@users.noreply.github.com>
2021-04-14 19:44:30 +0100

    cardsSlice implemented

--- src/features/quizzes/Quiz.js
+++ src/features/quizzes/Quiz.js
@@ -8,18 +8,25 @@ export default function Topic() {
   const quizzes = useSelector(selectQuizzes);
   let { quizId } = useParams();
   const quiz = quizzes[quizId];
-
-  return (
-    <section>
-      <h1>{quiz.name}</h1>
-      <ul className="cards-list">
-        {quiz.cardIds.map((id) => (
-          <Card key={id} id={id} />
-        ))}
-      </ul>
-      <Link to={ROUTES.newQuizRoute()} className="button center">
-        Create a New Quiz
-      </Link>
-    </section>
-  );
+  if (quiz) {
+    return (
+      <section>
+        <h1>{quiz.name}</h1>
+        <ul className="cards-list">
+          {quiz.cardIds.map((id) => (
+            <Card key={id} id={id} />
+          ))}
+        </ul>
+        <Link to={ROUTES.newQuizRoute()} className="button center">
+          Create a New Quiz
+        </Link>
+      </section>
+    );
+  } else {
+    return (
+      <p style={{ textAlign: "center", fontWeight: "bold", fontSize: 22 }}>
+        Add a quiz please
+      </p>
+    );
+  }
 }
